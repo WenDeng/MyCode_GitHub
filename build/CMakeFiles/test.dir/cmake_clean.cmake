93be0bd7fddc7a0ebb90293a7e54cb62fadc5bd0
dengwen <201722060935@std.uestc.edu.cn>
2019-04-20 12:59:14 +0800

    update gitignore

---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/sum.cpp.o"
+  "CMakeFiles/test.dir/demo/main.cpp.o"
+  "../bin/test.pdb"
+  "../bin/test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
