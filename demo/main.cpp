0d6a08cc07bde7d43dcab5c352429c3d73646bff
dengwen <201722060935@std.uestc.edu.cn>
2019-04-30 20:10:25 +0800

    update

--- demo/main.cpp
+++ demo/main.cpp
@@ -10,33 +10,25 @@ public:
     void FindNumsAppearOnce(vector<int> data,int* num1,int *num2) {
         //也不知道是谁想到的这种方法，反正很牛逼
         int result=0,k=1;
+        if(data.size()&1==1) return;//单数个数表示不合法
+        
         for(auto foo:data) result^=foo;
         while(!(result&k)) k=k<<1;//找到result不为0的最低位
-
-        int i=0,j=data.size()-1;
-        while(i<j)
+        
+        if(!result) return;//表示所有的数都是成对出现的，也不合法
+        for(auto foo:data) 
         {
-            while(data[i]&k) i++;//找到第一个最低为不为1的数的下标
-            while(!(data[j]&k)) j--;//找到第一个最低为不为0的数的下标
-            if(i<j)
-            { 
-                swap(data[i],data[j]);//交换
-                i++;j--;
-            }
+            if(foo&k) *num1= (*num1)^foo;//对应位为1
+            else *num2= (*num2)^foo;//对应为0
         }
-        int t=i-1;
-        while(t>=0) *num1= (*num1)^data[t--];//前面部分
-        while(i<data.size()) *num2= (*num2)^data[i++];//后面部分
     }
 };
-
-
 int main()
 {
    int value[10]={1,2,3,4,5,5,4,4,5,6};
-//    vector<int> data={1,2,3,4,5,4,3,5};
+   vector<int> data={1,2,3,4,5,4,3,5};
 //    vector<int> data={1,2};
-   vector<int> data={1,3,4,5,4,3,5,2};
+//    vector<int> data={1,3,4,5,4,3,5,2};
    int res1=0,res2=0;
    Solution sol;
    sol.FindNumsAppearOnce(data,&res1,&res2);
