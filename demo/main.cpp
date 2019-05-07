751fc6411ec4eea344ba40261ed7389ffded74c2
dengwen <201722060935@std.uestc.edu.cn>
2019-05-07 22:30:14 +0800

    update

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,22 +8,74 @@
 #include <queue>
 using namespace std;
 
-//priority queue的底层容器是vector,其没有迭代器，
+
+class Base
+{
+public:
+    Base(int k)
+    {
+        p=new int(1);
+        q=*p=k;
+    }
+    // Base(Base &B)
+    // {
+    //     cout<<"copy constructor"<<endl;
+    //     *p=*B.p;
+    // }
+    ~Base()
+    {
+        delete p;
+    }
+    void show()
+    {
+        cout<<"q address: "<<&q<<endl;
+        cout<<"p address: "<<p<<endl;
+    }
+private:
+    int q;
+    int *p;
+};
+
 int main()
 {
-    int a[20]={1,2,3,4,5,5,6,7,8,9};
-    vector<int> vec(a,a+11); 
+    cout << endl << "常规变量-----------------------------------------" << endl;
+    int k = 6, s = 7;
+    cout << k << " " << s << endl;
+    k=std::move(s);
+    cout << k << " " << s << endl;
+    k=8;
+    cout<<k<<endl;
 
-    priority_queue<int> prique(vec.begin(),vec.end());
+    cout << endl << "常规数组-----------------------------------------" << endl;
+    vector<int> data1 = {1, 2};
+    vector<int> data2 = {1, 3, 4, 5, 4, 3, 5, 2};
+    data1 = std::move(data2);
+//     data1 = static_cast<vector<int> &&>(data2);
+    cout<< "after move:" << endl<< "data1:";
+    for (int foo : data1)  cout << foo << " ";
 
-    prique.push(23);
-    prique.push(24);
-    cout<<"size: "<<prique.size()<<endl;
-    while(!prique.empty())
-    {
-        cout<<prique.top()<<" ";
-        prique.pop();
-    }
-    cout<<endl;
+    cout << endl<< "data2:";
+    for (int foo : data2)  cout << foo << " ";
+
+    cout<< endl << endl << "指针变量-----------------------------------------" << endl;
+    int m = 3, n = 5;
+    int *p = &m, *q = &n;
+    p = std::move(q);
+    cout<<p<<"       "<<*p<<endl;
+    cout<<q<<"       " << *q << endl;
+
+    cout << endl << "class 对象-----------------------------------------" << endl;
+    Base ba(5);
+    Base bb(2);
+    bb=std::move(ba);
+    ba.show();
+    bb.show();
+
+    cout << endl << "string -----------------------------------------" << endl;
+    string str=std::move("deng wen");
+    string str1("luo chao");
+    str1=std::move(str);
+    cout<<&str<<"   "<<str<<endl;
+    cout<<&str1<<"   "<<str1<<endl;
     return 0;
 }
