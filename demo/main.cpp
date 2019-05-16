65df6230097c1bb046a1b216ab3e10ea763f8459
dengwen <201722060935@std.uestc.edu.cn>
2019-05-17 00:34:39 +0800

    update

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,80 +8,34 @@
 #include <queue>
 using namespace std;
 
-
-class Base
+class Solution
 {
 public:
-    Base(int k)
+    bool IsContinuous(vector<int> numbers) //求是否位顺子
     {
-        p=new int(1);
-        q=*p=k;
-    }
+        if(numbers.size()!=5) return false;
+        int count=0,sum=0,i,j;
+        for(i=0;i<5;i++) //这里进行选择排序
+        for(j=i+1;j<5;j++) if (numbers[i] > numbers[j])  swap(numbers[i], numbers[j]); 
 
-    ~Base()
-    {
-        delete p;
+        for(i=0;i<4;i++) 
+        {
+            if(numbers[i]==0) count++; //记录大小王的个数
+            else if(numbers[i]==numbers[i+1]) return false;//如果有两值相等，肯定不行
+            else sum+=numbers[i+1]-numbers[i]; //记录差之和
+        }
+        cout<<count<<"  "<<sum<<endl;
+        if(sum<5 && (sum-count)<5) return true; 
+        return false;
     }
-    void show()
-    {
-        cout<<"q address: "<<&q<<endl;
-        cout<<"p address: "<<p<<endl;
-    }
-private:
-    int q;
-    int *p;
 };
 
 int main()
 {
-    cout << endl << "常规变量-----------------------------------------" << endl;
-    int k = 6, s = 7;
-    cout << k << " " << s << endl;
-    k=std::move(s);
-    cout << k << " " << s << endl;
-    k=8;
-    cout<<k<<endl;
-
-    cout << endl << "常规数组-----------------------------------------" << endl;
-    vector<int> data1 = {1, 2};
-    vector<int> data2 = {1, 3, 4, 5, 4, 3, 5, 2};
-    data1 = std::move(data2);
-//     data1 = static_cast<vector<int> &&>(data2);
-    cout<< "after move:" << endl<< "data1:";
-    for (int foo : data1)  cout << foo << " ";
-
-    cout << endl<< "data2:";
-    for (int foo : data2)  cout << foo << " ";
-
-    cout<< endl << endl << "指针变量-----------------------------------------" << endl;
-    int m = 3, n = 5;
-    int *p = &m, *q = &n;
-    p = std::move(q);
-    cout<<p<<"       "<<*p<<endl;
-    cout<<q<<"       " << *q << endl;
-
-    cout << endl << "class 对象-----------------------------------------" << endl;
-    Base ba(5);
-    Base bb(2);
-    bb=std::move(ba);
-    ba.show();
-    bb.show();
-
-    cout << endl << "string -----------------------------------------" << endl;
-    string str=std::move("deng wen");
-    string str1("luo chao");
-    str1=std::move(str);
-    cout<<&str<<"   "<<str<<endl;
-    cout<<&str1<<"   "<<str1<<endl;
-
-    cout << endl << "vector -----------------------------------------" << endl;
-    vector<int> vec1={1,2,3,4,5};
-    vector<int> vec2={9,0,9};
-    vec1=std::move(vec2);
-    for (int foo : vec1)  cout << foo << "  ";cout<<endl;
-    for (int i=0;i<vec1.size();i++)  cout << &vec1[i] << "   ";
-    cout<<endl;
-    
-    
+    vector<vector<int> > ret;
+    Solution solu;
+    vector<int> data = {1, 2, 3, 0, 0};
+    vector<int> data1 = {0, 0, 2, 1, 5};
+    cout<<"result: "<<boolalpha<<solu.IsContinuous(data1)<<endl;
     return 0;
 }
