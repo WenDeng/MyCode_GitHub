93ddad704688765f28bdc6c2d9287e14b20aec83
deng wen <2364233124@qq.com>
2019-05-17 16:35:02 +0800

    update

--- demo/main.cpp
+++ demo/main.cpp
@@ -5,23 +5,49 @@
 #include <sstream>
 #include <set>
 #include <map>
-
+#include <string>
 using namespace std;
+
+class Solution
+{
+public:
+    Solution(){m_data=0;}
+    Solution(int k):m_data(k){cout<<"constructor"<<endl;}
+    Solution(Solution &so){cout<<"copy construcotr"<<endl; m_data=so.m_data;}
+
+    string LeftRotateString(string str)
+    {
+        int i, j, index = 0, start = 0, len = str.length();
+        cout<<"str length: "<<len<<endl;
+        while(index<len && start<len) 
+        {
+            while(index<len && str[index]!=' ') index++;
+            cout<<"start:"<<start<<" end:"<<index-1<<"    str:";
+            for(i=start;i<index;i++) cout<<str[i];
+            cout<<endl;
+            for(i=start,j=index-1;i<j;i++,j--) swap(str[i],str[j]);//翻转每一个子字符串
+            start=++index;//新的起始位置
+        }
+
+        for(i=0,j=len-1;i<j;i++,j--) swap(str[i],str[j]);
+        return str;        
+    } 
+private:
+    int m_data;
+};
 
 int main()
 {
-    cout<<"set usage-------------------------------"<<endl;
-    int value[5]={1,2,3,4,5};
-    set<int> data(value,value+5);
-    data.insert(3);
-    data.erase(4);
-    set<int>::iterator iter=data.begin();
-    for(;iter!=data.end();iter++) cout<<*iter<<" ";
-
-    iter=data.find(5);//使用这个函数比使用STL的find函数更有效率
-    cout<<*iter<<endl;
-    // *iter=9;//企图通过迭代器修改set元素是不被允许的
-
-
+    // string str("wen deng am I");
+    // Solution solu;
+    // cout<<solu.LeftRotateString(str)<<endl;
+    // vector<int> vec={1,3,5,6,7,4,8};
+    // sort(vec.begin,vec.end(),less<int>());
+    // for(auto foo:vec) cout<<foo<<" ";
+    // cout<<endl;
+    for (char i = 0; i < 256; ++i)
+    {
+        printf("%d\n", i);
+    }
     return 0;
 }
