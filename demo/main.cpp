463b68771033368f614a62c84584db25792e6800
deng wen <2364233124@qq.com>
2019-05-27 20:25:35 +0800

    update

--- demo/main.cpp
+++ demo/main.cpp
@@ -10,41 +10,36 @@
 #include <functional>
 using namespace std;
 
-class Solution {
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Solution
+{
 public:
-    int StrToInt(string str)
-    { //返回值为0的几种情况区分，正负号、值范围保证2147483647~-2147483648，
-        if(str.length()==0) return 0;
-        int result=0,i,state=1;
-
-        if(str[0]<='9'&&str[0]>='0') result=result*10+str[0]-'0';
-        else if(str[0]=='+')  state=1;
-        else if(str[0]=='-')  state=-1;
-        else return 0;
-
-        for(i=1;i<str.length();i++)
+    bool match(char *str, char *pattern)
+    {
+        int i=0,j=0;
+        while(*str!='\n' && *pattern!='\n')
         {
-            if(str[i]>='0'&&str[i]<='9') result=result*10+str[i]-'0';
-            else return 0;
+            if()
         }
-
-        return state==-1?-result:result;
+        if(*str=='\n' && *pattern=='\n') return true;
+        return false;
     }
 };
-
 int main()
 {
-    // string str("wen deng am I");
-    // Solution solu;
-    // cout<<solu.LeftRotateString(str)<<endl;
-    // vector<int> vec={1,3,5,6,7,4,8};
-    // sort(vec.begin,vec.end(),less<int>());
-    // for(auto foo:vec) cout<<foo<<" ";
-    // cout<<endl;
     Solution solu;
-    int n=10;
-    cout<<"sum: "<<solu.StrToInt("d3234")<<endl;
-    cout << "sum: " << solu.StrToInt("-321") << endl;
-    cout << "sum: " << solu.StrToInt("+321") << endl;
-    return 0;
+    if(solu.match("aaa","a.a")) cout<<"match success!!"<<endl;
+    else cout<<"match fail!!!!"<<endl;
+
+    if(solu.match("aaa","ab*ac*a")) cout<<"match success!!"<<endl;
+    else cout<<"match fail!!!!"<<endl;
+
+    if(solu.match("aaa","aa.a")) cout<<"match success!!"<<endl;
+    else cout<<"match fail!!!!"<<endl;
+
+    if(solu.match("aaa","ab*a")) cout<<"match success!!"<<endl;
+    else cout<<"match fail!!!!"<<endl;
 }
