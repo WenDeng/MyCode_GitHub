fd39fe93e9e8bf63073a9568f13fec53afd354f0
deng wen <2364233124@qq.com>
2019-06-05 21:01:12 +0800

    update

--- code_set/practice/stl_string.cpp
+++ code_set/practice/stl_string.cpp
@@ -7,6 +7,7 @@ using namespace std;
 //C++中string、char *、char[]的转换
 int main()
 {
+    //----------------C++中string、char *、char[]的转换-------------------------------------------
     string str = "hello";//string to char*
     const char *p = str.data(); //加const  或者用char * p=(char*)str.data();的形式
     const char *q = str.c_str(); //同上，要加const或者等号右边用char*
@@ -19,6 +20,16 @@ int main()
     string s;
     char *cs = "hello"; //直接赋值
     s = cs; //直接将char转为string
+    //--------------------------------------------------------------------------------------------
+    
+    //------------------------------------整数和字符串之间的转换-----------------------------------
+    string myInfo;
+    int age=18,number=201322060935;
+    myInfo+="name: dengwen   ";
+    myInfo+="age: "+to_string(age)+"   ";
+    myInfo+="number: "+to_string(number);
+    cout<<myInfo<<endl;
 
-
+    int k=atoi("18");
+    cout<<k<<endl;
 }
--- demo/main.cpp
+++ demo/main.cpp
@@ -22,65 +22,51 @@ struct TreeNode {
 class Solution
 {
 public:
-    // void DoSerialize(string &str,TreeNode *root)
-    // {
-    //     if(root==NULL) str+="#";
-    //     DoSerialize(str,root->left);
-    //     str+=root->val;
-    //     DoSerialize(str,root->right);
-    //     cout<<str;
-    // }
-    // char* Serialize(TreeNode *root)
-    // {   //将二叉树转换为字符串
-    //     string str;
-    //     DoSerialize(str,root);
-
-    //     str="dengwen";
-    //     char *ret=new char[str.length()+1];
-    //     str.copy(ret,str.length(),0);//拷贝字符串
-    //     ret[str.length()]=='\0';
-    //     return ret;
-    // }
-
-    // TreeNode* Deserialize(char *str)
-    // { //将字符串转换为二叉树
-    // }
-
-    void midSearch(TreeNode *node, int &k, TreeNode **pp)
+    void DoSerialize(string &str,TreeNode *root)
     {
-        if (node == NULL) return;
-        if(node->left) midSearch(node->left, k, pp);
-        k--;
-        if (k == 0) *pp = node;
-        if(node->right) midSearch(node->right, k, pp);
+        if(root==NULL) {str+="#,";return;}
+        if(root) str += to_string(root->val) + ",";
+        DoSerialize(str,root->left);
+        DoSerialize(str,root->right);
     }
-    TreeNode* KthNode(TreeNode *pRoot, int k)
-    {
-        if (pRoot == NULL) return NULL;
-        TreeNode *ret;
-        midSearch(pRoot, k, &ret);//获取指针变量的地址
+    char* Serialize(TreeNode *root)
+    {   //将二叉树转换为字符串
+        string str;
+        DoSerialize(str,root);
+        char *ret=new char[str.length()];  
+        str.copy(ret,str.length()-1,0);//拷贝字符串
+        ret[str.length()-1]=='\0';
         return ret;
+    }
+
+    TreeNode* Deserialize(char *str)
+    { 
+        //将字符串转换为二叉树
     }
 };
 
-int gVar=4;
-int *pVar=&gVar;
-void setvalue(int **p)
-{
-    *p=pVar;
-}
+
 
 int main()
 {
     Solution solu;
-    TreeNode *p = new TreeNode(3);
-    // cout<<solu.Serialize(p)<<endl;
-    TreeNode *q=solu.KthNode(p,1);
-    cout<<q->val<<endl;
+    TreeNode *p = new TreeNode(11);
+    TreeNode *node1 = new TreeNode(2);
+    TreeNode *node2 = new TreeNode(3);
+    TreeNode *node3 = new TreeNode(6);
+    TreeNode *node4 = new TreeNode(16);
+    p->left=node3;
+    p->left->left=node2;
+    p->left->left->left=node1;
+    p->right=node4;
 
-    int **q1;
-    setvalue(q1);
-    cout<<**q1<<endl;
+    cout<<solu.Serialize(p)<<endl;
 
+    TreeNode *q=solu.Deserialize(solu.Serialize(p));
+    cout<<solu.Serialize(q)<<endl;
+    // string str="deng";
+    // string &str1=str;
+    // str1+=" wen";
+    // cout<<str1<<endl;
 
 }
