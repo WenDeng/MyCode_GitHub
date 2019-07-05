c6bd63b5a47556c68b9c5ea81ca7bb7c63a77354
dengwen <dengwen@qq.com>
2019-07-05 15:41:56 +0800

    update

--- code_set/practice/stl_priorityqueue_slist.cpp
+++ code_set/practice/stl_priorityqueue_slist.cpp
@@ -9,6 +9,103 @@
 #include <slist>
 
 using namespace std;
+/**
+默认比较函数为less, 大顶堆
+**/
+void defaultCmpLess() {
+    cout << "=========defaultCmpLess(big heap)========" << endl;
+    priority_queue<int> q;
+    for (int i = 0; i < 10; i++) {
+        q.push(rand()%20);
+    }
+
+    while (!q.empty()) {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    cout <<endl;
+
+}
+
+/**
+使用greater比较函数，小顶堆
+**/
+void useCmpGreater() {
+    cout << "=========useCmpGreater(small heap)========" << endl;
+    priority_queue<int, vector<int>, greater<int> > q;
+    for (int i = 0; i < 10; i++) {
+        q.push(rand()%20);
+    }
+
+    while (!q.empty()) {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    cout <<endl;
+}
+
+
+//==========================
+
+struct Node
+{
+    int x, y;
+    Node(int a = 0, int b = 0):x(a), y(b){};
+    friend bool operator<(const Node &a, const Node &b);
+
+};
+inline bool operator<(const Node &a, const Node &b) {
+    if (a.x != b.x)  return a.x < b.x;
+    return a.y > b.y;
+}
+
+/**
+运算符重载
+**/
+void overloadOperator() {
+    cout << "=========overload Operator< =========" << endl;
+    cout << "a.x < b.x; a.y > b.y" << endl;
+    priority_queue<Node> q;
+    for (int i = 0; i < 10; i++) {
+        q.push( Node( rand()%20, rand()%20 ) );
+    }
+
+    while (!q.empty()) {
+        cout << q.top().x << "," << q.top().y << endl;
+        q.pop();
+    } 
+    cout << endl;   
+}
+
+
+//=========================
+/**
+自构建比较函数
+**/
+struct cmp2
+{
+    bool operator()(int a, int b) {
+        return a < b;
+    }
+};
+
+void designCmp() {
+    cout << "=========designCmp========" << endl;
+    cout << "operator a<b" << endl;
+    priority_queue<int, vector<int>, cmp2 > q;
+
+    for (int i = 0; i < 10; i++) {
+        q.push(rand()%20);
+    }
+
+    while (!q.empty()) {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    cout <<endl;
+}
+
+
 
 //priority queue的底层容器是vector,其没有迭代器，
 int main()
--- demo/main.cpp
+++ demo/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <deque>
 #include <queue>
 #include <memory>
 #include <sstream>
@@ -14,44 +15,59 @@ using namespace std;
 
 class Solution {
 public:
-    string longestPalindrome(string s) 
+    static bool myCompare(vector<int> &value1,vector<int> &value2)
     {
-        if(s.length()<=1) return s;//大小为1的字符串必为回文串
+        if(value1[0]==value2[0]) return value1[1]<=value2[1];
+        return value1[0]<value2[0];
+    }
 
-        string rev=s,ret,temp;
-        reverse(rev.begin(),rev.end());//翻转字符串
-        if(rev==s) return s;
-        int len=0;//存放回文子串的长度
-        for(int i=0;i<s.length();i++)//查找s与rev的最长公共子串
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        // 动态规划+二分查找求解
+        // （1）先将信封进行排序，主键宽度由小到大，为了方便二分查找，高度由大到小
+        // （2）用二分法依次从前往后求出每个信封对应的最大套数（对应为最大递增子序列）
+        if(!envelopes.size()) return 0;
+        sort(envelopes.begin(),envelopes.end(),[](vector<int> &va1,vector<int> &va2){
+            if(va1[0]==va2[0]) return va1[1]>va2[1]; //保证前面的大值被后面的小值冲掉，相当于消除了宽相等而高较大的值
+            return va1[0]<va2[0];} //用lamda表达式实现了函数
+            );//进行排序
+        
+        for(int i=0;i<envelopes.size();i++)  cout<<"["<<envelopes[i][0]<<","<<envelopes[i][1]<<"]    ";
+        cout<<endl;
+
+        int envsize=envelopes.size(),len=0;
+        vector<int> envque(envsize,0);
+        for(int i=0;i<envsize;i++)
         {
-            temp="";//存放待验证子串
-            for(int j=i;j<s.length();j++)
-            {
-                temp+=s[j];//提取子串，每次增加一个字符
-                if(temp.length()<len)  continue;//子串的长度不够....
-                else if(rev.find(temp)!=-1)//在rev中找到temp
-                {
-                    string q=temp;//q用来验证temp是否是回文子串
-                    reverse(q.begin(),q.end());
-                    if(q==temp)
-                    {
-                        len=temp.length();
-                        ret=temp;
-                    }
-                }
-                else break;
-            }
+            int temp=envelopes[i][1];
+            auto iter=lower_bound(envque.begin(),envque.begin()+len,temp); //这个算法用的是二分查找，寻找第一个不小于temp的迭代器
+            if(iter==(envque.begin()+len)) envque[len++]=temp;
+            else *iter=temp;
         }
-        return ret;
+
+        for(int i=0;i<len;i++)  cout<<envque[i]<<" ";
+        cout<<endl;
+        return len;
     }
 };
 
 
+//priority queue的底层容器是vector,其没有迭代器，
 int main()
 {
-    Solution solu;
-    cout<<"result:"<<solu.longestPalindrome("abbb")<<endl;
-    cout<<"result:"<<solu.longestPalindrome("aacdefcaa")<<endl;
-    cout<<"result:"<<solu.longestPalindrome("ohomm")<<endl;
-    cout<<"result:"<<solu.longestPalindrome("asdfghnmzxcv")<<endl;
+    int a[20]={1,2,3,4,5,5,6,7,8,9};
+    vector<int> vec(a,a+11); 
+
+    //优先级队列
+    priority_queue<int,vector<int>,greater<int> > prique(vec.begin(),vec.end()); //优先级队列
+    prique.push(23);
+    prique.push(24);
+    cout<<"priority queue size: "<<prique.size()<<endl;
+    while(!prique.empty())
+    {
+        cout<<prique.top()<<" ";
+        prique.pop();
+    }
+    cout<<endl;
+
+    return 0;
 }
